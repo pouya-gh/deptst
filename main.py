5f046b9145e738062a788df99e048adb638af972
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-07-01 10:21:40 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+import getpass
+
+if __name__ == "__main__":
+    with open("output.txt", "w") as f:
+        f.write(f"Hello, {getpass.getuser()}! (time {datetime.now()})")
